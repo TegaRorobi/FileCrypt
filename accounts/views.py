f29a7ebd8da93a63f41ab7a1fef8d0d5b971a930
Tega Rorobi <rorobitega@gmail.com>
2025-02-03 00:52:26 +0100

    bugfix: Fix bug with UsersViewSet.get_permisions and UsersViewSet.get_object
    Made sure get_object still ran the super class's method when there was a pk to query, made sure only superusers can list or make changes to a specific user queried by pk

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets, mixins, status, permissions
 from django.contrib.auth import get_user_model
-from .serializers import (
-    UserSerializer,
-)
+from accounts.serializers import (
+    UserSerializer)
+from utils.permissions import (
+    IsSuperUser)
 
-viewsets.ModelViewSet
 
 User = get_user_model()
 
@@ -19,12 +19,26 @@ class UsersViewSet(
 
     queryset = User.objects.order_by('-id')
     serializer_class = UserSerializer
+    lookup_url_kwarg = 'pk'
 
 
     def get_permissions(self):
-        if self.action in ('create', 'list'):
+        # anyone can access the 'create' method
+        if self.action == 'create':
             return [permissions.AllowAny()]
-        return  [permissions.IsAuthenticated()]
+        # retrieving all users, and/or a specific user can only be done by a superuser
+        elif self.action == 'list' or self.lookup_url_kwarg in self.kwargs:
+            return  [IsSuperUser()]
+        # actions related to the currently authenticated user
+        else:
+            return [permissions.IsAuthenticated()]
+
+
 
     def get_object(self):
-        return self.request.user
+        # if there's a pk in the url, run the super class's default method
+        if self.lookup_url_kwarg in self.kwargs:
+            return super().get_object()
+        # else, return the currently authenticated user
+        else:
+            return self.request.user
