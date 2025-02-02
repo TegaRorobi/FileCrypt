a209f50503306ea585b6d814f141dd5c417433e8
Tega Rorobi <rorobitega@gmail.com>
2025-02-02 23:23:35 +0100

    feat: Create ViewSet for User model; make use of UserSerializer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status, permissions
+from django.contrib.auth import get_user_model
+from .serializers import (
+    UserSerializer,
+)
 
-# Create your views here.
+viewsets.ModelViewSet
+
+User = get_user_model()
+
+
+class UsersViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin):
+
+    queryset = User.objects.order_by('-id')
+    serializer_class = UserSerializer
+
+
+    def get_permissions(self):
+        if self.action in ('create', 'list'):
+            return [permissions.AllowAny()]
+        return  [permissions.IsAuthenticated()]
+
+    def get_object(self):
+        return self.request.user
