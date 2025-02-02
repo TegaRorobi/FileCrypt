848fa4388495a6bae7bce892216a80fbb0efeee6
Tega Rorobi <rorobitega@gmail.com>
2025-02-02 23:26:10 +0100

    feat: Add 'accounts' app urlconf to filecrypt.urls; set up Swagger schema and urls

--- filecrypt/urls.py
+++ filecrypt/urls.py
@@ -1,22 +1,43 @@
 """
 URL configuration for filecrypt project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    info = openapi.Info(
+        title="FileCrypt API",
+        default_version='v1',
+        description="FileCrypt API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@django.site"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes = (AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+
+    # rest_framework (mainly auth)
+    path('', include('rest_framework.urls')),
+
+    # swagger/redoc
+    re_path('^api/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(), name='schema-json'),
+    re_path('^api/swagger/?$', schema_view.with_ui('swagger'), name='schema-swagger'),
+    re_path('^api/redoc/?$', schema_view.with_ui('redoc'), name='schema-redoc'),
+
+    # media/static
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
