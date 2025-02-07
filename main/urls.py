6517600bf65c4d3a96435897415d2549db00d20b
Tega Rorobi <rorobitega@gmail.com>
2025-02-07 23:16:09 +0100

    feat: Configure urls for BusinessViewSet in 'main' urlconf

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from main.views import (
     EncryptedResourceViewSet,
     EncryptionKeyViewSet,
+    BusinessViewSet,
 )
 from utils.aliases import *
 
@@ -12,4 +13,9 @@ urlpatterns = [
     re_path('^encrypted-resources/d/(?P<pk>\d+)/?$', EncryptedResourceViewSet.as_view({'post': 'decrypt_resource'}), name='decrypted-resource-detail'),
 
     re_path('^encryption-key/?$', EncryptionKeyViewSet.as_view({'post': 'retrieve'})),
+
+    re_path('businesses/list/?$', BusinessViewSet.as_view(LIST), name='businesses-list'),
+    re_path('businesses/?$', BusinessViewSet.as_view(CREATE), name='businesses-create'),
+    re_path('businesses/me/?$', BusinessViewSet.as_view(RETRIEVE_UPDATE_DESTROY), name='businesses-detail-me'),
+    re_path('businesses/(?P<pk>\d+)/?$', BusinessViewSet.as_view(RETRIEVE_DESTROY), name='business-detail'),
 ]
