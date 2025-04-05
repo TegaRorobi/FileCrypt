53d5e7f65d50104e67bffb7baa8919afcc7d4561
Tega Rorobi <rorobitega@gmail.com>
2025-04-05 23:32:07 +0100

    feat: Register models in admin dashboard (basic)

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from main.models import *
 
-# Register your models here.
+admin.site.register(EncryptedResource)
+admin.site.register(ResourceAccess_User)
+admin.site.register(EncryptionKey)
+
+admin.site.register(Business)
+
+admin.site.register(Organisation)
+admin.site.register(OrganisationMember)
+admin.site.register(ResourceAccess_Organisation)
+
+admin.site.register(Workspace)
+admin.site.register(WorkspaceMember)
+admin.site.register(ResourceAccess_Workspace)
+
+admin.site.register(Team)
+admin.site.register(TeamMember)
+admin.site.register(ResourceAccess_Team)
