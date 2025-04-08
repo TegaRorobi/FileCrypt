1b9f8f026ab091b5dea541c7ced3637c2ca466b9
Tega Rorobi <rorobitega@gmail.com>
2025-04-08 15:07:20 +0100

    feat: Improve the decrypt_file method of the EncryptionManager to better handle the input file name and also return the filename.extension value

--- utils/crypt.py
+++ utils/crypt.py
@@ -21,21 +21,29 @@ class EncryptionManager:
             encryption_key: str,
             http_origin: str):
         f = Fernet(encryption_key)
+        decrypted_file_name = input_file.name.split('/')
         decrypted_data = f.decrypt(input_file.read())
         input_file.close()
 
+        """
+        decrypted_file_name.split('/')[1:-1] # the folder paths except 'uploads/' and  the end, 'filename.extension'
+        decrypted_file_name.split('/')[-1] # filename.extension
+        """
+
         curr_path = 'media/'
-        # Create the necessary directories in the input file's name attribute.
-        for folder_name in ['decrypted'] + input_file.name.split('/')[:-1]:
+        # Creating the necessary directories in the input file's name attribute.
+        directories_to_create = decrypted_file_name[1:-1] if len(decrypted_file_name) > 2 else []
+        for folder_name in ['decrypted'] + directories_to_create:
             curr_path += folder_name + '/'
             if not os.path.exists(settings.BASE_DIR / curr_path):
                 os.mkdir(settings.BASE_DIR / curr_path)
 
-        decrypted_file_path = settings.BASE_DIR / f'media/decrypted/{input_file.name}'
-        decrypted_file_url = http_origin + os.path.join(settings.MEDIA_URL, f'decrypted/{input_file.name}')
+        decrypted_file_path = settings.BASE_DIR / f'media/decrypted/{decrypted_file_name[-1]}'
+        decrypted_file_url = http_origin + os.path.join(settings.MEDIA_URL, f'decrypted/{decrypted_file_name}')
 
         file = open(decrypted_file_path, 'wb+')
         file.write(decrypted_data)
         file.close()
 
-        return decrypted_file_url, decrypted_file_path, decrypted_data
+
+        return decrypted_file_name[-1], decrypted_file_url, decrypted_file_path, decrypted_data
